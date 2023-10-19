c76b2457c59b7c9b46f52c2167f0831aaac3e406
mangabriel03 <mangabriel03@gmail.com>
2023-10-19 13:17:23 +0300

    Fara Teste

---
+++ src/Main/LargeNumber.java
@@ -0,0 +1,91 @@
+package Main;
+
+public class LargeNumber {
+//    public int size;
+//    public int[] zahl;
+
+    //    LargeNumber(int size2, int[] arr){
+//        size = size2;
+//        zahl = new int[size];
+//        zahl = arr;
+//    }
+    public int[] sum(int[] nr1, int[] nr2) {
+        int[] result = new int[nr1.length];
+        int temp = 0;
+        for (int i = nr1.length - 1; i >= 0; i--) {
+            result[i] = (nr1[i] + nr2[i] + temp) % 10;
+            temp = (nr1[i] + nr2[i] + temp) / 10;
+        }
+        if (temp > 0) {
+            result = extend(result, temp);
+        }
+        return result;
+    }
+
+    public int[] extend(int[] number, int carry) {
+        int[] result = new int[number.length + 1];
+        result[0] = carry;
+        for (int i = 0; i < number.length; i++) {
+            result[i + 1] = number[i];
+        }
+        return result;
+    }
+
+    public int[] dif(int[] nr1, int[] nr2) {
+        int[] result = new int[nr1.length];
+        int temp = 0;
+        int dif = 0;
+        for (int i = nr1.length - 1; i >= 0; i--) {
+
+            dif = nr1[i] - nr2[i] - temp;
+            if (dif < 0) {
+                dif = dif + 10;
+                temp = 1;
+            } else
+                temp = 0;
+
+            result[i] = dif;
+        }
+        return result;
+    }
+
+    public int[] mul(int[] nr1,int nr2) {
+        int[] result = new int[nr1.length];
+        int temp = 0;
+        for (int i = nr1.length - 1; i >= 0; i--){
+            if (nr1[i] * nr2 > 10){
+                temp = nr1[i] * nr2 / 10;
+                result[i] = nr1[i] * nr2 %10;
+            }
+            else
+            {
+                result[i] = nr1[i] * nr2 + temp;
+                temp = 0;
+            }
+        }
+        return result;
+    }
+
+    public int[] div(int[] nr1, int nr2) {
+        int[] result = new int[nr1.length];
+        int temp = 0;
+        for (int i = 0; i <= nr1.length - 1; i++){
+
+            int digit = nr1[i] + temp * 10;
+            result[i] = digit / nr2;
+            temp = digit % nr2;
+        }
+        int k = 0;
+        while(k < result.length && result[k] == 0)
+            k ++;
+        if (k > 0)
+        {
+            int[] corectResult = new int[result.length - k];
+            for (int i = 0; i < corectResult.length ; i++)
+                corectResult[i] = result[i + k];
+            return corectResult;
+        }
+        else
+            return result;
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,53 @@
+//package Main;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+package Main;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        NotenProzessor notenProzessor = new NotenProzessor();
+
+        int[] noten = new int[] { 10, 20, 30, 39, 47, 48, 60 };
+
+        System.out.println(Arrays.toString(notenProzessor.nichtAusreichendeNoten(noten)));
+        System.out.println(notenProzessor.Durchschnittswert(noten));
+        System.out.println(Arrays.toString(notenProzessor.abgerundeteNote(noten)));
+        System.out.println(notenProzessor.Maximale(noten));
+        Operations op = new Operations();
+        System.out.println(op.getMax());
+        System.out.println(op.getMin());
+        System.out.println(op.minSum());
+        System.out.println(op.maxSum());
+        int[] nr1Array = {1,0,3,0,5,8,0,7,0,1};
+        int[] nr2Array = {5,0,0,9,5,3,1,4,1,2};
+        LargeNumber nr1 = new LargeNumber();
+        System.out.println(Arrays.toString(nr1.sum(nr1Array, nr2Array)));
+        System.out.println(Arrays.toString(nr1.dif(nr1Array, nr2Array)));
+        int nr3 = 3;
+        System.out.println(Arrays.toString(nr1.mul(nr1Array, nr3)));
+        System.out.println(Arrays.toString(nr1.mul(nr2Array, nr3)));
+        int[] n = {1,3,9};
+        System.out.println(Arrays.toString(nr1.div(n,nr3)));
+        System.out.println(Arrays.toString(nr1.div(nr1Array,nr3)));
+        Shop shop = new Shop();
+        int[] tastaturen = {40, 35, 70, 15, 45};
+        int[] usb = {15, 20, 10, 35};
+        int budget = 60;
+        System.out.println(shop.billigsteTastatur(tastaturen));
+        System.out.println(shop.teuersteGegenstand(tastaturen, usb));
+        System.out.println(shop.teuersteUsb(usb, budget));
+        Arrays.sort(tastaturen);
+        System.out.println(Arrays.toString(tastaturen));
+        Arrays.sort(usb);
+        System.out.println(Arrays.toString(usb));
+        System.out.println(shop.geldbetrag(tastaturen, usb, budget));
+    }
+
+}
